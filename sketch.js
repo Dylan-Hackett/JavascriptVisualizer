ee2119f32cf7299e0c56f5dc1dae10ca4eed9af5
Dylan Hackett <86651230+Dylan-Hackett@users.noreply.github.com>
2023-06-17 23:47:32 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,127 +1,149 @@
 let song;
 let dusty = [];
+let dustyLarge = [];
+let dustyExtraLarge = [];
 let input;
+let beginning = false;
+let warningDisplayed = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
-  
-  // Get the file input element
   input = select('#fileinput');
-
-  // Attach an event listener that calls loadFile when the user selects a file
   input.changed(loadFile);
 }
 
 function loadFile() {
-  // If there's a previous song playing, stop it
   if (song) {
     song.stop();
   }
 
-  // Load the selected file
-  song = loadSound(input.elt.files[0], songLoaded);
+  song = loadSound(input.elt.files[0], () => {
+    warningDisplayed = true;
+    redraw();
+  });
 }
-
-function songLoaded() {
-  song.loop();
-
-  // Create a new FFT analyzer
-  song.fft = new p5.FFT();
-
-  // Remove the file input
-  input.remove();
+function drawShape(t, indexFunction, rFunction, xFunction, yFunction) {
+  beginShape();
+  for(let i = 0; i<=180; i++) {
+    let index = indexFunction(i);
+    let r = rFunction(index);
+    let x = xFunction(i, r, t);
+    let y = yFunction(i, r);
+    point(x, y);
+  }
+  endShape();
 }
-
+function handleParticles(particles, ParticleClass, amplitude) {
+  if (particles.length < 1000) { // Limit particle count
+    let d = new ParticleClass();
+    particles.push(d);
+  }
+  
+  for(let i = particles.length - 1; i >= 0; i--){
+    if (!particles[i].edges()){
+      particles[i].update(amplitude, amplitude > 220);
+      particles[i].show();
+    } else {
+      particles.splice(i, 1);
+    }
+  }
+}
 
 function draw() {
-  if (!song) {
-    // If no song is loaded yet, don't try to visualize it
+  if (warningDisplayed) {
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text("Warning: Flashing lights. Click to start. (BRIGHTNESS UP!)", width / 2, height / 2);
+    noLoop();
+    return;
+  }
+  
+  if (song && !song.isPlaying()) {
     return;
   }
 
-  background(0);
-  stroke(0,240,15);
+  background(0);  
+  stroke(0,255,0); 
   noFill();
-
   translate(width/2, height/2);
-
   song.fft.analyze();
-  let amplitude = song.fft.getEnergy(20,300);
-  let amplitude_mid = song.fft.getEnergy(1000,6000);
+  
   let wave = song.fft.waveform();
-  let rotation = song.currentTime();
-  rotation *= 5
+  let amplitude_mid = song.fft.getEnergy(1000,6000);
+  let amplitude = song.fft.getEnergy(20,300);
+  let rotation = song.currentTime()*5;
+  rotation*=5
   rotate(rotation);
 
-  for (let t = -1; t<= 1; t+=2){
-    // First shape
-    beginShape();
-    for(let i = 0; i<=180; i++) {
-      let index = floor(map(i,0,180,0,wave.length-1));
-      let r = map(wave[index]*(.7*Math.abs(amplitude_mid)),0,1,100,650);
-      let x = (r * -sin(i) * t)*1.3*(.7*Math.abs(amplitude_mid));
-      let y = (r * cos(i*amplitude)%80)*1.3;
-      point(x,y);
-    }
-    endShape();
+  for (let t = -1; t <= 1; t += 2) {
+    drawShape(t,
+      i => floor(map(i,0,180,0,wave.length-1)),
+      index => map(wave[index]*(.7*Math.abs(amplitude_mid)),0,1,100,650),
+      (i, r, t) => (r * -sin(i) * t)*1.3*(.7*Math.abs(amplitude_mid)),
+      (i, r) => (r * cos(i*amplitude)%80)*1.3
+    );
 
-    // Second shape
-    beginShape();
-    for(let i = 0; i<=180; i++) {
-      let index = floor(map(i,0,180,0,wave.length-1))%20;
-      let r = map(wave[index]*(.7*Math.abs(amplitude_mid)),-1,1,150,350);
-      let x = (r * -sin(i) * t%80)*2.4;
-      let y = (r * cos(i))*1.4;
-      point(x,y);
-    }
-    endShape();
+    drawShape(t,
+      i => floor(map(i,0,180,0,wave.length-1))%20,
+      index => map(wave[index]*(.7*Math.abs(amplitude_mid)),-1,1,150,350),
+      (i, r, t) => (r * -sin(i) * t%80)*2.4,
+      (i, r) => (r * cos(i))*1.4
+    );
 
-    // Third shape
-    beginShape();
-    for(let i = 0; i<=180; i++) {
-      let index = floor(map(i,0,180,0,wave.length-1))%10;
-      let r = map(wave[index]*(.7*Math.abs(amplitude_mid)),-1,1,150,350);
-      let x = (r * -sin(i) * t%80)*1.9;
-      let y = (r * cos(i))*1.4;
-      point(y,x);
-    }
-    endShape();
+    drawShape(t,
+      i => floor(map(i,0,180,0,wave.length-1))%10,
+      index => map(wave[index]*(.7*Math.abs(amplitude_mid)),-1,1,150,750),
+      (i, r, t) => (r * -sin(i) * t%80)*1.9,
+      (i, r) => (r * cos(i))*1.4
+    );
 
-    // Fourth shape
-    beginShape();
-    for(let i = 0; i<=180; i++) {
-      let index = floor(map(i,-1,180,1,wave.length-1))%70;
-      let r = map(wave[index]*(.7*Math.abs(amplitude_mid)),-1,1,10,350);
-      let x = (r * -sin(i) * t)*2.;
-      let y = (r * cos(i))*2.;
-      point(x,y);
-    }
-    endShape();
+    drawShape(t,
+      i => floor(map(i,-1,180,1,wave.length-1))%70,
+      index => map(wave[index]*(.7*Math.abs(amplitude_mid)),-1,1,10,450),
+      (i, r, t) => (r * -sin(i) * t)*2,
+      (i, r) => (r * cos(i))*2
+    );
   }
-  if (dusty.length < 1000) { // Limit particle count
-    let d = new Dust();
-    dusty.push(d);
-  }
+  handleParticles(dusty, Dust, amplitude);
+  handleParticles(dustyLarge, DustLarge, amplitude);
+  handleParticles(dustyExtraLarge, DustExtraLarge, amplitude);
+}
 
-  for(let i = dusty.length - 1; i >= 0; i--){
-    if (!dusty[i].edges()){
-      dusty[i].update(amplitude > 200); // use 'amplitude' here instead of 'amp'
-      dusty[i].show();
-    } else {
-      dusty.splice(i, 1);
-    }
+function mouseClicked() {
+  if (warningDisplayed) {
+    warningDisplayed = false;
+    song.loop();
+    song.fft = new p5.FFT();
+    input.remove();
+    loop();
+
+  } else if (song && song.isPlaying()) {
+    song.pause();
+    noLoop();
+
+  } else if (song) {
+    song.play();
+    loop();
   }
 }
 
 class Dust {
-  constructor(){
-    this.position = p5.Vector.random2D().mult(250);
+  constructor(radiusMultiplier = 1, size = 1){  // default values added
+    radiusMultiplier = (typeof radiusMultiplier === 'number' && isFinite(radiusMultiplier)) ? radiusMultiplier : 1;
+    let randomValue = random(0.0001,0.0001);
+    randomValue = (typeof randomValue === 'number' && isFinite(randomValue)) ? randomValue : 0.0001;
+    this.position = p5.Vector.random2D().mult(radiusMultiplier);
     this.velocity = createVector(0,0);
-    this.accelerate = this.position.copy().mult(random(0.0001,0.0001));
+    this.accelerate = this.position.copy().mult(randomValue);
+    this.alpha = 255; //New property for fading effect
+    this.size = size;
   }
 
-  update(cond){
+  update(amplitude, cond){
+    let amp = map(amplitude, 0, 255, 0, 5); // Scale the amplitude to a suitable range
+    let randomChange = p5.Vector.random2D().mult(random(0.1)).mult(amp);
+    this.accelerate.add(randomChange);
     this.velocity.add(this.accelerate);
     this.position.add(this.velocity);
     if (cond){
@@ -129,25 +151,34 @@ class Dust {
       this.position.add(this.velocity);
       this.position.add(this.velocity);
     }
+    this.alpha -= 0.001; //Decrease alpha over time
   }
 
   edges(){
-    return this.position.x < -width/2 || this.position.x > width/2 || this.position.y < -height/2 || this.position.y > height/2;
+    return this.position.x < -width/2 || this.position.x > width/2 || this.position.y < -height/2 || this.position.y > height/2 || this.alpha < 0;
   }
 
-  show(){
+  show() {
     noStroke();
-    fill(0,240,105);
-    ellipse(this.position.x,this.position.y, 4);
+    fill(0,255,0,this.alpha);
+    ellipseMode(CENTER);
+    ellipse(this.position.x, this.position.y, this.size, this.size);
   }
 }
 
-function mouseClicked() {
-  if (song && song.isPlaying()) {
-    song.pause();
-    noLoop();
-  } else if (song) {
-    song.play();
-    loop();
+class DustLarge extends Dust {
+  constructor(){
+    super(400, 4); // passing the different parameters to the super class
   }
 }
+class DustExtraLarge extends Dust {
+  constructor(){
+    super(550, 6); // passing the different parameters to the super class
+  }
+}
+
+
+
+
+
+
